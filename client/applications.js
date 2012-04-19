210e1ce002f53919585b6c05b3b73476c97c2abe
Xavier Antoviaque <xavier@antoviaque.org>
2012-04-18 21:01:57 -0400

    Ported tags feature from Meteor TODO example. Now individual candidatures can be assigned tags from the admin.

--- client/applications.js
+++ client/applications.js
@@ -1,14 +1,26 @@
 
+
+// DB Collections ////////////////////////////////////////////////////////
+
 Applications = new Meteor.Collection("applications_hackers");
 Meteor.subscribe('applications_hackers');
 
+
+// Session variables /////////////////////////////////////////////////////
+// Text from the search input field
 Session.set('search_text', '');
+
+// _id of the application for which a new tag is being added
+Session.equals('editing_addtag', null);
+
+// Utils /////////////////////////////////////////////////////////////////
+
+// Formatting //
 
 function urlize(text) {
     var list = text.match(/\b(http:\/\/|www\.|http:\/\/www\.)[^ \n\r<]{2,200}\b/g);
     if(list) {
         for(i = 0; i < list.length; i++) {
-            console.log(list[i]);
             var prot = list[i].indexOf('http://') === 0 || list[i].indexOf('https://') === 0 ? '' : 'http://';
             text = text.replace( list[i], "<a target='_blank' href='" + prot + list[i] + "'>"+ list[i] + "</a>" );
         }
@@ -37,6 +49,52 @@ function format_db_applications(applications) {
     return formatted;
 }
 
+// In-place editing //
+
+// Returns an event_map key for attaching "ok/cancel" events to
+// a text input (given by selector)
+var okcancel_events = function (selector) {
+    return 'keyup '+selector+', keydown '+selector+', focusout '+selector;
+};
+
+// Creates an event handler for interpreting "escape", "return", and "blur"
+// on a text field and calling "ok" or "cancel" callbacks.
+var make_okcancel_handler = function(options) {
+    var ok = options.ok || function() {};
+    var cancel = options.cancel || function() {};
+
+    return function(evt) {
+        if(evt.type === "keydown" && evt.which === 27) {
+            // escape = cancel
+            cancel.call(this, evt);
+
+        } else if(evt.type === "keyup" && evt.which === 13 ||
+                  evt.type === "focusout") {
+            // blur/return/enter = ok/submit if non-empty
+            var value = String(evt.target.value || "");
+            if(value) {
+                ok.call(this, value, evt);
+            } else {
+                cancel.call(this, evt);
+            }
+        };
+    };
+};
+
+// Finds a text input in the DOM by id and focuses it.
+var focus_field_by_id = function(id) {
+    var input = document.getElementById(id);
+    if(input) {
+        input.focus();
+        input.select();
+    }
+};
+
+
+// Templates /////////////////////////////////////////////////////////////
+
+// {{> applications }}
+
 Template.applications.results = function () {
     var search_text = Session.get('search_text');
     if(search_text === '') {
@@ -55,6 +113,24 @@ Template.applications.results = function () {
     return format_db_applications(applications);
 };
 
+// {{> application_item }}
+
+Template.application_item.tag_objs = function () {
+    var application_id = this._id;
+    return _.map(this.tags || [], function (tag) {
+        return {application_id: application_id, tag: tag};
+    });
+};
+
+Template.application_item.adding_tag = function () {
+    return Session.equals('editing_addtag', this._id);
+};
+
+
+// Events ////////////////////////////////////////////////////////////////
+
+// {{> search }}
+
 Template.search.events = {
     'keyup #search-input': function(evt) {
         if(evt.type === "keyup" && evt.which === 13 || evt.type === "focusout") {
@@ -62,4 +138,42 @@ Template.search.events = {
         }
     },
 }
+
+// {{> application_item }}
+
+Template.application_item.events = {
+    'click .addtag': function(evt) {
+        Session.set('editing_addtag', this._id);
+        Meteor.flush(); // update DOM before focus
+        focus_field_by_id("edittag-input");
+    },
+};
+
+Template.application_item.events[okcancel_events('#edittag-input')] =
+    make_okcancel_handler({
+        ok: function (value) {
+            Applications.update(this._id, {$addToSet: {tags: value}});
+            Session.set('editing_addtag', null);
+        },
+        cancel: function () {
+            Session.set('editing_addtag', null);
+        }
+    });
+
+// {{> application_tag }}
+
+Template.application_tag.events = {
+    'click .remove': function (evt) {
+        var tag = this.tag;
+        var id = this.application_id;
+
+        evt.target.parentNode.style.opacity = 0;
+        // wait for CSS animation to finish
+        Meteor.setTimeout(function () {
+            Applications.update({_id: id}, {$pull: {tags: tag}});
+        }, 300);
+   }
+};
+
+
 
